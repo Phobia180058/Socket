ada8fae5544d47eb7b62d26b10e573134306acd2
Phobia180058 <916732746@qq.com>
2017-03-24 17:07:11 +0800

    Create JSONDemo.java
    简单使用JSON包来解析和编码

---
+++ JSONDemo.java
@@ -0,0 +1,31 @@
+package JSON;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import net.sf.json.JSONObject;
+
+
+public class JsonDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HashMap map = new HashMap();
+         map.put("userID", "100");
+         map.put("name", "zhangsan");
+         map.put("Date", new String[]{"2013","2014"});
+
+         JSONObject json = JSONObject.fromObject(map);
+         String data = json.toString();
+         System.out.println(data);
+         
+         JSONObject object = JSONObject.fromObject(data);
+         String userID = object.getString("userID");
+         System.out.println(userID);
+         
+         
+         
+	}
+
+}
